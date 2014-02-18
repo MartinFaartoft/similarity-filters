b3e41a2683ac4d1395d64ec1e8edc298c855226c
RickMarker <rickmarker@gmail.com>
2014-02-18 14:50:51 +0100

    Made it possible to generate data

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,1 +1,19 @@
-#TODO generate random bitvectors (args: how long, how many) and pipe them to std::out
+# TODO generate random bitvectors (args: how long, how many) and pipe them to std::out
+
+from sys import argv
+import random
+import json
+
+array_length = int(argv[1])
+no_of_arrays = int(argv[2])
+
+arrays = []
+
+for i in range(no_of_arrays):
+    array = []
+    for j in range(array_length):
+        array.append(random.randint(0, 1))
+
+    arrays.append(array)
+
+print json.dumps(arrays)
