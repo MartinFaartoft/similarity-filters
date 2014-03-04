e35a0c644eb3b641a207e468a7d0bb0e80bedc7e
Marcus Bjerg Gregersen <mabg@40c.dk>
2014-03-04 15:15:33 +0100

    Defining variables from harvard pp. 44

--- src/statistics.py
+++ src/statistics.py
@@ -1,35 +1,49 @@
 from dsbf import DistanceSensitiveBloomFilter
 import random
+import math
 
 randBinList = lambda n: [random.randint(0, 1) for b in range(1, n+1)]
 
-k = 5
-m = 5
-bits_to_sample = 2
 prime_100 = 2147483659
 seed = 9859028509821
-threshold = 4
-
-length = 10
-
-number_of_elements = 10
+length = 21
+number_of_elements = 1000
 number_of_candidates = 100
-closeness = 0
-
 own_seed = 8585
+#Harvard pp. 44
+# c = Number of characters in the "alphabet"
+c = 1
+epsilon = 0.1
+delta = 0.4
+k = 10
+
+n = number_of_elements
+
+base = (1 - c * epsilon) / float(1- c * delta)
+# l_prime = number of bits to sample
+l_prime = int(math.ceil(math.log(4 * n, base)))
+# m_prime = Size of each of the k subarrays (total memory usage m = k * m_prime)
+m_prime = 2 ** l_prime
+
+threshold = (k * ((1 - c * epsilon) ** l_prime)) / 2
+
+closeness = int(math.floor(length * epsilon))
+
+print "values", l_prime, m_prime, threshold, closeness
+
 
 
 def generate_close_candidates(candidate, close, number_of_candidates):
     """ candidate = candidate bit array
         close = number of bits to flip """
     #Generate bit mask
+    random.seed(seed)
     zeroes = [0] * len(candidate)
     for i in range(close):
         zeroes[i] = 1
-    random.seed(seed)
+
 
     for i in range(number_of_candidates):
-
         random.shuffle(zeroes)
         copy = list(candidate)
         for u in range(len(zeroes)):
@@ -38,23 +52,23 @@ def generate_close_candidates(candidate, close, number_of_candidates):
         yield copy
 
 
+
 def populate_dsbf(dsbf, n, length):
     #TODO: Insert static random data
     for i in range(n):
         element = randBinList(length)
         dsbf.add_element(element)
-        print dsbf.bit_array
 
 
 def run():
-    dsbf = DistanceSensitiveBloomFilter(k, m, bits_to_sample, prime_100, seed, threshold, length)
+    dsbf = DistanceSensitiveBloomFilter(k, m_prime, l_prime, prime_100, seed, threshold, length)
     populate_dsbf(dsbf, number_of_elements, length)
     candidate = randBinList(length)
     count_true = 0
     for close_element in generate_close_candidates(candidate, closeness, number_of_candidates):
+        print dsbf.count_number_of_true_values(close_element)
         if dsbf.is_close(close_element):
             count_true += 1
-
     return count_true
 
 
