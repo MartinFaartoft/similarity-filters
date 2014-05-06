fb72d4efdf3d2d97f2ee65662705b84a7482a71d
Martin Faartoft <martin.faartoft@gmail.com>
2014-05-06 11:38:33 +0200

    W experiments

--- src/crunch_wildcard_stats.py
+++ src/crunch_wildcard_stats.py
@@ -1,44 +1,121 @@
 import json
+import math
 #data looks like: (k, space, total_input_size, fraction, data_row)
 def avg(l):
     return sum(l) / float(len(l))
 
 data = []
-with open('wildcard/harvard_graph_with_wildcards_50.json', 'r') as datafile:
+with open('wildcard/work_over_wildcards_50.json', 'r') as datafile:
     data = json.load(datafile)
 
 #print len(data)
-#one frame for each step_k
-frame = data[0]
+#one frame for each step_w
+frame = data[7]
 #frame is a dictionary with reasonable keys
-k, space, total_input_size, fraction, l, k, n, l_prime = frame['parameters']
+k, space, total_input_size, fraction, l, k, n, l_prime, number_of_candidates = frame['parameters']
 print l_prime, l, k
 
-close_stats = frame['close_stats']
-#print close_stats
-print len(close_stats), "should be 100 (number of candidates)"
-print len(close_stats[0]), "should be 10, 20 or 30 (k)"
-print len(close_stats[0][0]), "should be 2, (worst_case, actual)"
-worst_sum = []
-worst_max = []
-worst_avg = []
-actual_sum = []
-actual_max = []
-actual_avg = []
-for i in range(len(close_stats)):
-    stats_for_candidate = close_stats[i]
-    #print stats_for_candidate
-    
-    worst_list = [worst for worst, actual in stats_for_candidate]
-    actual_list = [actual for (worst, actual) in stats_for_candidate]
-    
-    worst_sum.append(sum(worst_list))
-    worst_max.append(max(worst_list))
-    worst_avg.append(avg(worst_list))
-    
-    actual_sum.append(sum(actual_list))
-    actual_max.append(max(actual_list))
-    actual_avg.append(avg(actual_list))
-    
 
-print sorted(actual_avg)
+#print close_stats
+#print len(close_stats), "should be 1000 (number of candidates)"
+#print len(close_stats[0]), "should be 10, 20 or 30 (W)"
+#print len(close_stats[0][0]), "should be 2, (worst_case, actual)"
+
+x = []
+y_naive_worst = []
+y_naive_worst_log = []
+y_close_smart_worst = []
+y_close_smart_worst_log =[]
+y_close_smart_actual = []
+y_close_smart_actual_log = []
+
+y_far_smart_worst = []
+y_far_smart_worst_log =[]
+y_far_smart_actual = []
+y_far_smart_actual_log = []
+
+for frame in data:
+    close_worst_sum = []
+    close_worst_max = []
+    close_worst_avg = []
+    close_actual_sum = []
+    close_actual_max = []
+    close_actual_avg = []
+
+    far_worst_sum = []
+    far_worst_max = []
+    far_worst_avg = []
+    far_actual_sum = []
+    far_actual_max = []
+    far_actual_avg = []
+
+    close_stats = frame['close_stats']
+    far_stats = frame['far_stats']
+    for i in range(len(close_stats)):
+        far_stats_for_candidate = far_stats[i]#close_stats[i]
+        close_stats_for_candidate = close_stats[i]
+        #print stats_for_candidate
+        
+        worst_list = [worst for worst, actual in close_stats_for_candidate]
+        actual_list = [actual for (worst, actual) in close_stats_for_candidate]
+
+        close_worst_sum.append(sum(worst_list))
+        close_worst_max.append(max(worst_list))
+        close_worst_avg.append(avg(worst_list))
+        
+        close_actual_sum.append(sum(actual_list))
+        close_actual_max.append(max(actual_list))
+        close_actual_avg.append(avg(actual_list))
+
+        worst_list = [worst for worst, actual in far_stats_for_candidate]
+        actual_list = [actual for (worst, actual) in far_stats_for_candidate]
+
+        far_worst_sum.append(sum(worst_list))
+        far_worst_max.append(max(worst_list))
+        far_worst_avg.append(avg(worst_list))
+        
+        far_actual_sum.append(sum(actual_list))
+        far_actual_max.append(max(actual_list))
+        far_actual_avg.append(avg(actual_list))
+        #contains 1000 integers, one for each candidate
+        
+
+    #create 3 data points from this
+    w = len(frame['wildcards'])
+    x.append(w)
+    y_naive_worst_log.append(math.log(2 ** w, 2))
+
+    y_close_smart_worst_log.append(math.log(avg(close_worst_max), 2))
+    y_close_smart_actual_log.append(math.log(avg(close_actual_max), 2))
+
+    y_far_smart_worst_log.append(math.log(avg(far_worst_max), 2))
+    y_far_smart_actual_log.append(math.log(avg(far_actual_max), 2))
+
+    y_naive_worst.append(2 ** w)
+
+    y_close_smart_worst.append(avg(close_worst_max))
+    y_close_smart_actual.append(avg(close_actual_max))
+
+    y_far_smart_worst.append(avg(far_worst_max))
+    y_far_smart_actual.append(avg(far_actual_max))
+
+#plot that thing
+#print y_smart_actual
+
+from pylab import *
+
+
+figure(1)
+#a, = plot(x, y_naive_worst_log, 'k')
+b, = plot(x, y_close_smart_worst_log, 'k')
+c, = plot(x, y_close_smart_actual_log, 'k--')
+#d, = plot(x, y_far_smart_worst, 'g')
+e, = plot(x, y_far_smart_actual_log, 'k:')
+
+#legend([a,b], ['0', '1000'])
+xlabel('W')
+ylabel('Query time')
+#ylim(0,0.030)
+title('Query time as a function of wildcards')
+savefig('work_over_wildcards.eps')
+show()
